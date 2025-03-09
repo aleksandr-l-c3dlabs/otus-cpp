0435f940c2c4b01350a91e3f51310f18ea24906c
Alexander Lupandin <aleksandr.lupandin@c3dlabs.com>
2025-03-09 13:41:58 +0300

    Basic repo template

---
+++ cmake/macros.cmake
@@ -0,0 +1,68 @@
+# Задача
+MACRO(_task target)
+    add_executable(${target} main.cpp)
+    add_library(${target}_lib lib.cpp)
+
+    set_target_properties(${target} ${target}_lib PROPERTIES
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED ON
+    )
+
+    target_include_directories(${target}_lib
+        PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}"
+        PRIVATE "${CMAKE_CURRENT_BINARY_DIR}"
+    )
+
+    target_link_libraries(${target}
+        PRIVATE ${target}_lib
+    )
+
+    if (MSVC)
+        target_compile_options(${target} PRIVATE
+            /W4
+        )
+        target_compile_options(${target}_lib PRIVATE
+            /W4
+        )
+    else ()
+        target_compile_options(${target} PRIVATE
+            -Wall -Wextra -pedantic -Werror
+        )
+        target_compile_options(${target}_lib PRIVATE
+            -Wall -Wextra -pedantic -Werror
+        )
+    endif()
+
+    install(TARGETS ${target} RUNTIME DESTINATION bin)
+ENDMACRO()
+
+
+# Добавить новый тест в сборку
+MACRO(_test target sources lib)
+    add_executable(${target} ${sources})
+
+    set_target_properties(${target} PROPERTIES
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED ON
+    )
+
+    target_link_libraries(${target}
+        ${lib}
+        GTest::gtest_main
+    )
+
+    if (MSVC)
+        target_compile_options(${target} PRIVATE
+            /W4
+        )
+    else ()
+        target_compile_options(${target} PRIVATE
+            -Wall -Wextra -pedantic -Werror
+        )
+    endif()
+
+    add_test(NAME ${target} COMMAND ${target})
+ENDMACRO()
+
+
+
---
+++ tests/01-version/test_version.cpp
@@ -0,0 +1,9 @@
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+#include <lib.h>
+
+TEST(lib, version_positive) {
+  EXPECT_TRUE(version() > 0);
+}

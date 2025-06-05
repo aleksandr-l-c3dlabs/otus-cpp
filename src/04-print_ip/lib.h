15fcb365a36ef2863112b4054ca1b6a53a137acb
Alexander Lupandin <aleksandr.lupandin@c3dlabs.com>
2025-06-05 12:19:27 +0300

    Task 4: Fix std::string, std::vector, std::list templates

--- src/04-print_ip/lib.h
+++ src/04-print_ip/lib.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <list>
 #include <string>
 #include <tuple>
+#include <vector>
 
 void example_usage();
 
@@ -18,12 +20,40 @@ void print_ip(const T& value) {
   std::cout << std::endl;
 }
 
-void print_ip(const std::string& value) {
-  std::cout << value << std::endl;
-}
+template <typename T>
+struct is_string : std::false_type {};
 
 template <typename T>
-auto print_ip(const T& value) -> decltype(value.begin(), value.end(), void()) {
+struct is_string<std::basic_string<T>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_string_v = is_string<T>::value;
+
+template <typename T, std::enable_if_t<is_string_v<T>, bool> = true>
+void print_ip(const T& value) {
+  std::cout << value << std::endl;
+}
+template <typename T>
+struct is_list : std::false_type {};
+
+template <typename T>
+struct is_list<std::list<T>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_list_v = is_list<T>::value;
+
+template <typename T>
+struct is_vector : std::false_type {};
+
+template <typename T>
+struct is_vector<std::vector<T>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_vector_v = is_vector<T>::value;
+
+template <typename T,
+          std::enable_if_t<is_list_v<T> || is_vector_v<T>, bool> = true>
+auto print_ip(const T& value) {
   bool is_first = true;
   for (const auto& part : value) {
     if (!is_first)

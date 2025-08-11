371b4507beccae78c9645550f370907d42d99b95
Alexander Lupandin <aleksandr.lupandin@c3dlabs.com>
2025-08-11 12:39:13 +0300

    Task 12: Increasing the accuracy of calculations

--- src/12-mapreduce/reducer_avg/lib.cpp
+++ src/12-mapreduce/reducer_avg/lib.cpp
@@ -1,9 +1,9 @@
 #include "lib.h"
 
 double get_avg(std::istream& input) {
-  double value, avg = 0, n = 0;
+  long double value, avg = 0, n = 0;
   while (input >> value) {
-    avg = (value + n * avg) / (n + 1);
+    avg += (value - avg) / (n + 1);
     n++;
   }
   return avg;
